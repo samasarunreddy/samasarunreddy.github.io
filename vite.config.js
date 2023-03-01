d9d5314ac0fa230d3d23b4046adc3b1c05bc301d
Sarunnanimasai <103571317+Sarunnanimasai@users.noreply.github.com>
2023-03-01 18:46:15 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,11 @@ module.exports = {
   root: "src",
   build: {
     outDir: "../dist",
+    assetsInclude: ["**/*.pdf"],
   },
   resolve: {
     alias: {
       "@": path.join(__dirname, "src"),
     },
-  },
-  assets: {
-    // include all PDF files in the src/assets/pdf directory
-    include: "src/assets/pdf/**/*.pdf",
-    // set the output directory to dist/assets/pdf
-    outputDir: "assets/pdf",
   },
 };
