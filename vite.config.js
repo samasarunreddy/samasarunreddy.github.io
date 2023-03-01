c24ad812fdc65c82ba89aa879606856cc16ab9d3
Sarunnanimasai <103571317+Sarunnanimasai@users.noreply.github.com>
2023-03-01 15:02:08 +0530

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,17 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  root: 'src',
+  root: "src",
   build: {
-    outDir: '../dist'
+    outDir: "../dist",
   },
   resolve: {
     alias: {
-      '@': path.join(__dirname, 'src')
-    }
-  }
+      "@": path.join(__dirname, "src"),
+    },
+  },
+  assets: {
+    include: "src/img/docs/fw19_0637-Sama-Sarun-Reddy-Resume.pdf",
+    // set the output directory to dist/assets/pdf
+    outputDir: "assets/pdf",
+  },
 };
